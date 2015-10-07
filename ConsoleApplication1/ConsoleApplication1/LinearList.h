c49089bd499ac956f47e2136f750e21bd2ddf4d1
DengrongGuan12 <1031212913@qq.com>
2015-10-07 16:28:05 +0800

    SeqList

--- ConsoleApplication1/ConsoleApplication1/LinearList.h
+++ ConsoleApplication1/ConsoleApplication1/LinearList.h
@@ -7,6 +7,18 @@ public:
 	~LinearList();
 	virtual int Size()const = 0;
 	virtual int Length()const = 0;
+	virtual int Search(T& x)const = 0;
+	virtual int Locate(int i)const = 0;
+	virtual bool getData(int i, T& x)const = 0;
+	virtual void setData(int i, T& x) = 0;
+	virtual bool Insert(int i, T& x) = 0;
+	virtual bool Remove(int i, T& x) = 0;
+	virtual bool isEmpty()const = 0;
+	virtual bool isFull()const = 0;
+	virtual void Sort() = 0;
+	virtual void input() = 0;
+	virtual void output() = 0;
+	virtual LinearList<T> operator=(LinearList<T>& L) = 0;
 
 };
 
